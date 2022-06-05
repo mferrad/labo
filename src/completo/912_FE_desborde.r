6f1ca06392b3cf657fd9537b03f802eb2ad7f7cb
mferrad <martin.e.ferradas@gmail.com>
2022-06-05 18:10:10 -0300

    Baseline creation of all pipeline files

--- src/completo/912_FE_desborde.r
+++ src/completo/912_FE_desborde.r
@@ -270,7 +270,7 @@ AgregarVariables  <- function( dataset )
   {
     cat( "ATENCION, hay", nans_qty, "valores NaN 0/0 en tu dataset. Seran pasados arbitrariamente a 0\n" )
     cat( "Si no te gusta la decision, modifica a gusto el programa!\n\n")
-    dataset[mapply(is.nan, dataset)] <<- 0
+    dataset[mapply(is.nan, dataset)] <<- NA
   }
 
   ReportarCampos( dataset )
